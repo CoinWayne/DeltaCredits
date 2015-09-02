be96ce4f20780bb248a2e6aeb115680b600a678b
CoinWayne <coinwayne@gmail.com>
2015-09-03 03:17:28 +0500

    PoS reward changed

--- src/main.cpp
+++ src/main.cpp
@@ -991,6 +991,11 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
     int64_t nRewardCoinYear;
 
     nRewardCoinYear = MAX_MINT_PROOF_OF_STAKE;
+    
+    if (pindexBest->nHeight+1 > REWARD_FIX_BLOCK)
+    {
+       nRewardCoinYear = MAX_MINT_PROOF_OF_STAKE2;
+    }
 
     int64_t nSubsidy = nCoinAge * nRewardCoinYear / 365 / COIN;
 
